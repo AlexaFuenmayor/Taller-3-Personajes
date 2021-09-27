5fc71e51101642ea46b02cc9dac57a27a5f8358e
alexfuenmayor <estherf.m18@gmail.com>
2021-09-27 18:13:25 -0500

    primer commit

---
+++ animaciones.js
@@ -0,0 +1,35 @@
+let hechosHistoricos = () => {
+  alert(
+    "El 13 de agosto de 1805 Simón Bolivar viajó a París, donde se dedicó junto a su antiguo maestro Simón Rodríguez a la lectura de los clásicos y a ilustrarse en diversos campos del saber universal"
+  );
+};
+
+let hechosHistoricos1 = () => {
+  alert(
+    "Era 1810 y las colonias españolas estaban cada vez más descuidadas por el rey, fruto de las tensiones entre España y la Francia de Napoleón. Este año, Bolivar se unió a la revolución de Venezuela, pero el fracaso de la expedición le hizo abandonar su país."
+  );
+};
+
+let hechosHistoricos2 = () => {
+  alert(
+    "Año 1821 Participó en la fundación de la Gran Colombia, nación que intentó consolidar como una gran confederación política y militar en América, de la cual fue Presidente."
+  );
+};
+
+let hechosHistoricos3 = () => {
+  alert(
+    " Fue un discurso que el mundo no puede olvidar. El 28 de agosto de 1963, unas 250.000 personas marcharon hacia el monumento a Lincoln en Washington, En lo que llegaría a conocerse como el discurso “Tengo un sueño”, King expresó con pasión las exigencias del movimiento de derechos civiles de Estados Unidos: la igualdad de derechos para todos los ciudadanos, independientemente de su color de piel."
+  );
+};
+
+let hechosHistoricos4 = () => {
+  alert(
+    "King fue el principal militante de la no violencia en el movimiento de derechos civiles, protestando con éxito contra la discriminación racial en la legislación federal y estatal."
+  );
+};
+
+let hechosHistoricos5 = () => {
+  alert(
+    "El sueño de King de igualdad racial y lucha por la justicia trascendió las fronteras de Estados Unidos. Viajó por todo el mundo proclamando su visión de la “querida comunidad” y definiendo el racismo como un mal mundial. Fue galardonado con el Premio Nobel de la Paz en 1964."
+  );
+};
